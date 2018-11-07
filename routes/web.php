8fa828a98d8ac180607db8d7e63db54e91c53061
iamjunior <myjuniorprog@gmail.com>
2018-11-07 21:56:42 +0700

    video 2 & 3 One to One

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\User;
+use App\Profile;
+
+Route::get('/create_user', function (){
+    $user = User::create([
+        'name'      => 'Lufi',
+        'email'     => 'lufi.mahmud@gmail.com',
+        'password'  => bcrypt('password')
+    ]);
+
+    return $user;
+});
+
+Route::get('/create_profile', function (){
+    $profile = Profile::create([
+        'user_id'   => 1,
+        'phone' => '32141323',
+        'address'   => 'Jl. Anggrek Selatan Raya 3'
+    ]);
+
+    return $profile;
+});
+
+Route::get('/create_user_profile', function () {
+    $user = User::find(2);
+
+    $profile = new Profile([
+        'phone' => '024243234',
+        'address'   => 'Jalan Raya Sawo Matang 3'
+    ]);
+
+    $user->profile()->save($profile);
+    return $user;
+});
