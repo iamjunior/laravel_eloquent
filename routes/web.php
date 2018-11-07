3b68afaf17d8a3574306f43ea58e0624b3302635
iamjunior <myjuniorprog@gmail.com>
2018-11-07 22:55:34 +0700

    Video 4 Read Related Data

--- routes/web.php
+++ routes/web.php
@@ -49,3 +49,28 @@ Route::get('/create_user_profile', function () {
     $user->profile()->save($profile);
     return $user;
 });
+
+Route::get('/read_user', function () {
+    $user = User::find(1);
+
+    $data = [
+        'name'  => $user->name,
+        'phone' => $user->profile->phone,
+        'address'   => $user->profile->address
+    ];
+
+    return $data;
+});
+
+Route::get('/read_profile', function () {
+    $profile = Profile::where('phone', '024243234')->first();
+
+    // return $profile->user->name;
+    $data = [
+        'name'      => $profile->user->name,
+        'email'     => $profile->user->email,
+        'phone'     => $profile->phone,
+        'address'   => $profile->address
+    ];
+    return $data;
+});
